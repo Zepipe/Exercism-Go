704cec51671ac1ce8e64485639bc85fd61861d5f
Anton <akrilkrinov@gmail.com>
2026-02-14 17:36:38 +0300

    Update anagram.go
    Removing "reflect" package and improving readability

--- solutions/go/anagram/1/anagram.go
+++ solutions/go/anagram/1/anagram.go
@@ -1,7 +1,5 @@
 package anagram
-
 import ("unicode/utf8"
-        "reflect"
         "strings"
         )
 // func Detect is used to find anagrams for given subject word
@@ -12,19 +10,13 @@ func Detect(subject string, candidates []string) []string {
     
 	anagrams := make([]string, 0)
     lettersInSubject := make(map[rune]int)
-	subjectLength, candidateWordLength := 0, 0
+	subjectLength, candidateWordLength := utf8.RuneCountInString(subject), 0
     subject = strings.ToLower(subject) // to ignore case
     
     for _, letter := range subject { // Fill out the lettersInSubject map
-        _, exists := lettersInSubject[letter]
-        if !exists {
-            lettersInSubject[letter] = 1
-        } else {
-            lettersInSubject[letter]++
-        }
+        lettersInSubject[letter]++
     }
-
-    subjectLength = utf8.RuneCountInString(subject)
+	
     for _, word := range candidates {
         candidateWordLength = utf8.RuneCountInString(word)
         // if words are equal or has different length - skip
@@ -34,18 +26,27 @@ func Detect(subject string, candidates []string) []string {
         
         lettersInCandidateWord := make(map[rune]int) // Init for every word to compare
         for _, letter := range strings.ToLower(word) {
-            _, exists := lettersInCandidateWord[letter]
-            if !exists {
-                lettersInCandidateWord[letter] = 1
-            } else {
-                lettersInCandidateWord[letter]++
-            }
+            lettersInCandidateWord[letter]++
         }
         // check equality between subject word and candidate word
-        if reflect.DeepEqual(lettersInSubject, lettersInCandidateWord) {
+        if mapsEqual(lettersInSubject, lettersInCandidateWord) {
             anagrams = append(anagrams, word)
         }
     }
-
     return anagrams
+}
+
+// mapsEqual compares two rune frequency maps for equality.
+func mapsEqual(a, b map[rune]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	
+	for k, v := range a {
+		if b[k] != v {
+			return false
+		}
+	}
+	
+	return true
 }
