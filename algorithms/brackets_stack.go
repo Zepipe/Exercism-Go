dc092a268791ace5b534957bfe71abc232698992
Anton <akrilkrinov@gmail.com>
2026-02-16 16:55:14 +0300

    Create brackets_stack.go

---
+++ algorithms/brackets_stack.go
@@ -0,0 +1,34 @@
+package brackets
+
+func Bracket(input string) bool {
+    stack := []rune{}
+    
+    for _, char := range input {
+        switch char {
+        case '{', '[', '(':
+            // Push opening brackets onto the stack
+            stack = append(stack, char)
+            
+        case '}':
+            // Check if the top of the stack matches
+            if len(stack) == 0 || stack[len(stack)-1] != '{' {
+                return false
+            }
+            stack = stack[:len(stack)-1] // Pop from stack
+            
+        case ']':
+            if len(stack) == 0 || stack[len(stack)-1] != '[' {
+                return false
+            }
+            stack = stack[:len(stack)-1]
+            
+        case ')':
+            if len(stack) == 0 || stack[len(stack)-1] != '(' {
+                return false
+            }
+            stack = stack[:len(stack)-1]
+        }
+    }
+    
+    return len(stack) == 0
+}
